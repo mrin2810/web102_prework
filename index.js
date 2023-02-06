972335fa9951b8ecf25836bdcc3fed7de3e71667
Mrinmayee Rane <mrinmayeerane2810@gmail.com>
2023-02-06 02:05:30 -0500

    Challange 6 Code updated

--- index.js
+++ index.js
@@ -151,12 +151,16 @@ allBtn.addEventListener("click", showAllGames);
 const descriptionContainer = document.getElementById("description-container");
 
 // use filter or reduce to count the number of unfunded games
-
+const unfundedGamesCount = GAMES_JSON.filter(value => value.pledged < value.goal).length
 
 // create a string that explains the number of unfunded games using the ternary operator
-
+const fundraiserString = unfundedGamesCount ? `A total of $${totalRaised.toLocaleString('en-US')} has been raised for ${totalGames} games. Currently, ${unfundedGamesCount} ${unfundedGamesCount == 1 ? 'game remains' : 'games remain'} unfunded. We need your help to fund these amazing games!` : ``
 
 // create a new DOM element containing the template string and append it to the description container
+const displayMsg = document.createElement('p')
+displayMsg.innerHTML =  fundraiserString
+descriptionContainer.appendChild(displayMsg)
+
 
 /************************************************************************************
  * Challenge 7: Select & display the top 2 games
